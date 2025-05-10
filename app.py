c62c7f76d53a426eab5a5c4a979fe62414705770
lili59487 <a12151522@hotmail.com>
2025-05-11 02:06:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ CORS(app)
 DB_PATH = os.path.join(os.path.dirname(__file__), 'crop_usage.db')
 
 def get_db_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect('/data/crop_usage.db')
     conn.row_factory = sqlite3.Row
     return conn
 
